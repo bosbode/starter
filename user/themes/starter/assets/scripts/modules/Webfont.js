a72815d27de3a6d22cbfc6b803066312357debe8
Bosse Kemper <bosbode@gmail.com>
2018-02-15 14:11:13 +0100

    Add Font Awesome 5

--- user/themes/starter/assets/scripts/modules/Webfont.js
+++ user/themes/starter/assets/scripts/modules/Webfont.js
@@ -1,13 +1,18 @@
 import webfont from "webfontloader";
+var config = require('../../../../../../gulp/config.json');
 
 function Webfont(){
+	window.FontAwesomeConfig = {
+		searchPseudoElements: true
+	}
+
 	webfont.load({
     	google: {
       		families: ['Quicksand:400,400i,700,700i', 'Josefin+Slab:700']
 		},
 		custom: {
-			families: ['fontawesome'],
-			urls: ['user/themes/starter/assets/font-awesome/css/fontawesome.min.css']
+			families: ['Font Awesome 5 Solid', 'Font Awesome 5 Regular', 'Font Awesome 5 Brands', 'Font Awesome 5 Light'],
+			urls: ['../user/themes/' + config.theme + '/assets/font-awesome/css/fontawesome-all.min.css']
 		}
   	});
 }
