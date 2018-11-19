2cac962d084b3c4d83c6dcb993080ba3ac612b2c
Bosse Kemper <bosbode@gmail.com>
2018-11-19 13:39:25 +0100

    Update fullPage.js

--- user/themes/starter/assets/scripts/modules/FullPage.js
+++ user/themes/starter/assets/scripts/modules/FullPage.js
@@ -1,15 +1,42 @@
-import $ from "jquery";
-import fullpage from "fullpage.js";
+import $ from 'jquery';
+import fullpage from 'fullpage.js';
+import lottie from 'lottie-web';
 
 class FullPage {
 	constructor(){
-		this.fullpage = $("#fullpage");
+		this.fullpageId = '#fullpage';
 		this.initalise();
 	}
 
 	initalise() {
+		new fullpage(this.fullpageId, {
+			
+			licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+			verticalCentered: true,
+
+			afterLoad: function(origin, destination, direction){
+				var loadedSection = $(this);
+				
+				if(destination.index === 0){
+					setTimeout(function() {
+						lottie.play('logo');
+					}, 3000);
+				}
+			},
+			
+			onLeave: function(origin, destination, direction){
+				var leavingSection = $(this);
 		
-		this.fullpage.fullpage();
+				//after leaving section 1
+				if(origin.index === 0 && direction === 'down'){
+					lottie.stop('logo');
+				}
+		
+				else if(origin.index === 0 && direction === 'up'){
+					lottie.stop('logo');
+				}
+			}
+		});
 	}
 }
 
