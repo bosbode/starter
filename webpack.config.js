9604e99dc7710783b6c0837c36fd60ae5970fa8b
Bosse Kemper <bosbode@gmail.com>
2018-11-19 13:36:02 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,37 @@
 var path = require('path'),
 UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
-config = require("./gulp/config.json");
+config = require('./gulp/config.json');
 
 module.exports = {
+	mode: 'none',
 	entry: {
-		App: "./user/themes/" + config.theme + "/assets/scripts/App.js",
-		Vendor: "./user/themes/" + config.theme + "/assets/scripts/Vendor.js"
+		App: './user/themes/' + config.theme + '/assets/scripts/App.js',
+		Vendor: './user/themes/' + config.theme + '/assets/scripts/Vendor.js'
 	},
 	output: {
-		path: __dirname + "/user/themes/" + config.theme + "/assets/compiled/scripts",
-		filename: "[name].js"
+		path: __dirname + '/user/themes/' + config.theme + '/assets/compiled/scripts',
+		filename: '[name].js'
 	},
 	module: {
 		rules: [{ 
 			test: require.resolve('jquery'), 
 			use: [{ 
-			  loader: 'expose-loader', 
-			  options: 'jQuery' 
+				loader: 'expose-loader', 
+				options: 'jQuery' 
 			}, 
 			{ 
-			  loader: 'expose-loader', 
-			  options: '$' 
-			}] 
-		  }], 
-		loaders: [
-			{
-				loader: "babel-loader",
-				query: {
-					presets: ["es2015"]
-				},
-				test: /\.js$/,
-				exclude: /node_modules/
+				loader: 'expose-loader', 
+				options: '$' 
+			}],
+			test: /\.js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['es2015']
+				}
 			}
-		]
+		}]
 	},
 	plugins: [
     new UglifyJsPlugin()
