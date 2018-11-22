5bd1523604a2d17f941afece28690115103d5364
Bosse Kemper <bosbode@gmail.com>
2018-11-22 23:31:19 +0100

    Turn off bodymoving

--- user/themes/starter/assets/scripts/App.js
+++ user/themes/starter/assets/scripts/App.js
@@ -1,17 +1,17 @@
 import $ from "jquery";
-import BodyMovin from "./modules/BodyMovin";
+// import BodyMovin from "./modules/BodyMovin";
 import FullPage from "./modules/FullPage";
 import MobileMenu from "./modules/MobileMenu";
 // import StickyHeader from "./modules/StickyHeader";
 import Modal from "./modules/Modal";
 import Webfont from "./modules/Webfont";
-// import Slick from "./modules/Slick";
+import Slick from "./modules/Slick";
 import ObjectFit from "./modules/ObjectFit";
 
 
 var mobileMenu = new MobileMenu;
-// var slick = new Slick;
-var bodymovin = new BodyMovin;
+var slick = new Slick;
+// var bodymovin = new BodyMovin;
 var fullpage = new FullPage;
 var objectfit = new ObjectFit;
 // var stickyHeader = new StickyHeader;
--- user/themes/starter/assets/scripts/modules/FullPage.js
+++ user/themes/starter/assets/scripts/modules/FullPage.js
@@ -12,30 +12,30 @@ class FullPage {
 		new fullpage(this.fullpageId, {
 			
 			licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
-			verticalCentered: true,
+			verticalCentered: true
 
-			afterLoad: function(origin, destination, direction){
-				var loadedSection = $(this);
+			// afterLoad: function(origin, destination, direction){
+			// 	var loadedSection = $(this);
 				
-				if(destination.index === 0){
-					setTimeout(function() {
-						lottie.play('logo');
-					}, 3000);
-				}
-			},
+			// 	if(destination.index === 0){
+			// 		setTimeout(function() {
+			// 			lottie.play('logo');
+			// 		}, 3000);
+			// 	}
+			// },
 			
-			onLeave: function(origin, destination, direction){
-				var leavingSection = $(this);
+			// onLeave: function(origin, destination, direction){
+			// 	var leavingSection = $(this);
 		
-				//after leaving section 1
-				if(origin.index === 0 && direction === 'down'){
-					lottie.stop('logo');
-				}
+			// 	//after leaving section 1
+			// 	if(origin.index === 0 && direction === 'down'){
+			// 		lottie.stop('logo');
+			// 	}
 		
-				else if(origin.index === 0 && direction === 'up'){
-					lottie.stop('logo');
-				}
-			}
+			// 	else if(origin.index === 0 && direction === 'up'){
+			// 		lottie.stop('logo');
+			// 	}
+			// }
 		});
 	}
 }
