f62b2d7efde824e4e006e69a15fa678d1a372081
Bosse Kemper <bosbode@gmail.com>
2018-11-27 21:26:16 +0100

    Add critical task when working local

--- gulp/tasks/critical.js
+++ gulp/tasks/critical.js
@@ -11,3 +11,12 @@ gulp.task('critical', function () {
         dest: 'user/themes/' + config.theme + '/assets/compiled/styles/main.critical.css'
     });
 });
+
+gulp.task('criticalLocal', function () {
+    return critical.generate({
+        src: 'http://localhost:8080/',
+        width: 1920,
+        height: 1080,
+        dest: 'user/themes/' + config.theme + '/assets/compiled/styles/main.critical.css'
+    });
+});
