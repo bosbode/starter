7f262baedec4ba57ecbfda5016568ffc092d8494
Bosse Kemper <bosbode@gmail.com>
2018-11-27 11:46:35 +0100

    Change src for critical gulp task and add height and width

--- gulp/tasks/critical.js
+++ gulp/tasks/critical.js
@@ -3,9 +3,11 @@ critical = require('critical'),
 config = require("../config.json");
 
 
-gulp.task('critical', function () {
+gulp.task('critical', ['build'], function () {
     return critical.generate({
         src: config.criticalCSS,
+        width: 1920,
+        height: 1080,
         dest: 'user/themes/' + config.theme + '/assets/compiled/styles/main.critical.css'
     });
 });
