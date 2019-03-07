0e87cc04cfa2fe5e1f7db77cd3381d98b57f1b8b
Bosse Kemper <bosbode@gmail.com>
2019-03-07 12:58:37 +0100

    Correct series to parallel

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,5 @@ let hub = new HubRegistry(['gulp/tasks/*.js']);
 
 gulp.registry(hub);
 
-gulp.task('build', gulp.series(gulp.series('clearCompiled', 'clearCache'), gulp.parallel('modernizr', 'sass'), gulp.parallel('scripts', 'styles'), 'criticalCss'));
-gulp.task('default', gulp.series(gulp.series('clearCompiled', 'clearCache'), gulp.parallel('modernizr', 'sass'), gulp.parallel('scripts', 'styles'), 'watch'));
+gulp.task('build', gulp.series(gulp.parallel('clearCompiled', 'clearCache'), gulp.parallel('modernizr', 'sass'), gulp.parallel('scripts', 'styles'), 'criticalCss'));
+gulp.task('default', gulp.series(gulp.parallel('clearCompiled', 'clearCache'), gulp.parallel('modernizr', 'sass'), gulp.parallel('scripts', 'styles'), 'watch'));
