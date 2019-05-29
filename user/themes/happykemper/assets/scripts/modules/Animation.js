4d261ccaefa39bb2df5f724f417cf132d4fd270e
Bosse Kemper <bosbode@gmail.com>
2019-05-29 19:01:19 +0200

    Add favicons

--- user/themes/happykemper/assets/scripts/modules/Animation.js
+++ user/themes/happykemper/assets/scripts/modules/Animation.js
@@ -64,22 +64,20 @@ class Animations {
 	}
 
 	animateFish() {
-		const that = this;
-
 		// Hide bubbles
 		this.addOrRemoveClass(this.bubbles, 'animation--hide', 'add');
 
 		// Undo hide bubbles after 2400 milliseconds
-		setTimeout(function() {
-			that.addOrRemoveClass(that.bubbles, 'animation--hide', 'remove');
+		setTimeout(() => {
+			this.addOrRemoveClass(this.bubbles, 'animation--hide', 'remove');
 		}, 1400);
 
 		// Play fish animation
 		this.addOrRemoveClass(this.fish, 'fish-group--play-animation', 'add');
 		
 		// Remove fish animation so it can play again
-		setTimeout(function() {
-			that.addOrRemoveClass(that.fish, 'fish-group--play-animation', 'remove');
+		setTimeout(() => {
+			this.addOrRemoveClass(this.fish, 'fish-group--play-animation', 'remove');
 		}, 1200);
 	}
 
@@ -96,8 +94,6 @@ class Animations {
 	}
 	
 	flyBird() {
-		const that = this;
-
 		if (!this.treeClicked) {
 			// Show bird container and append animation, remove tree shake animation
 			this.addOrRemoveClass(this.birdContainer, 'animation--show', 'add');
@@ -105,17 +101,13 @@ class Animations {
 			this.addOrRemoveClass(this.birdTree, 'animation__tree', 'remove');
 	
 			// Hide bird container after animation is ended
-			this.addListenerMulti(this.birdContainer, 'webkitAnimationEnd animationend', function(){
-				that.addOrRemoveClass(that.birdContainer, 'animation--show', 'remove');
-			})
+			this.addListenerMulti(this.birdContainer, 'webkitAnimationEnd animationend', () => this.addOrRemoveClass(this.birdContainer, 'animation--show', 'remove'));
 
 			this.treeClicked = true;
 		}
 	}
 
 	nightMode() {
-		const that = this;
-
 		// Shows and appends tentacle animation
 		this.addOrRemoveClass(this.octopusTentacles, 'octopus-tentacles-group--play-animation', 'add');
 		this.addOrRemoveClass(this.octopusHead, 'octopus-head-group--play-animation', 'add');
@@ -130,27 +122,26 @@ class Animations {
 		this.addOrRemoveClass(this.treeShade, 'tree-shade--white', 'add');
 		this.addOrRemoveClass(this.tree, 'tree--blue', 'add');
 
-		// Inverts the colors so it looks like that these items are unaffected by the night swap
+		// Inverts the colors so it looks like this these items are unaffected by the night swap
 		this.addOrRemoveClass([this.contentClass, this.contentTitle, this.siteHeader, this.fireContainer, this.icon], 'content--invert', 'add');
 
 		// Undo all above changes after 2.5 seconds
-		setTimeout(function() {
-			that.addOrRemoveClass(that.octopusTentacles, 'octopus-tentacles-group--play-animation', 'remove');
-			that.addOrRemoveClass(that.octopusHead, 'octopus-head-group--play-animation', 'remove');
+		setTimeout(() =>  {
+			this.addOrRemoveClass(this.octopusTentacles, 'octopus-tentacles-group--play-animation', 'remove');
+			this.addOrRemoveClass(this.octopusHead, 'octopus-head-group--play-animation', 'remove');
 			
-			that.addOrRemoveClass([that.tentaclesContainer, that.octopusHeadContainer, that.night], 'animation--show', 'remove');
+			this.addOrRemoveClass([this.tentaclesContainer, this.octopusHeadContainer, this.night], 'animation--show', 'remove');
 			
-			that.addOrRemoveClass([that.bubbles, that.sun], 'animation--hide', 'remove');
+			this.addOrRemoveClass([this.bubbles, this.sun], 'animation--hide', 'remove');
 			
-			that.addOrRemoveClass(that.treeShade, 'tree-shade--white', 'remove');
-			that.addOrRemoveClass(that.tree, 'tree--blue', 'remove');
+			this.addOrRemoveClass(this.treeShade, 'tree-shade--white', 'remove');
+			this.addOrRemoveClass(this.tree, 'tree--blue', 'remove');
 
-			that.addOrRemoveClass([that.contentClass, that.contentTitle, that.siteHeader, that.fireContainer, that.icon], 'content--invert', 'remove');
+			this.addOrRemoveClass([this.contentClass, this.contentTitle, this.siteHeader, this.fireContainer, this.icon], 'content--invert', 'remove');
 		}, 2500);
 	}
 
 	valueOne() {
-		const that = this;
 		let animationDelayTotal = 2.2;
 		const animationDelayDeduct = 0.2;
 
@@ -165,12 +156,12 @@ class Animations {
 		animationDelayTotal -= animationDelayDeduct;
 		
 		this.addOrRemoveClass(this.popup, 'animation__value-01-pop-up--play-animation', 'add');
-		this.popup.forEach(function(item, index, arr) {
+		this.popup.forEach(item => {
 			item.style.animationDelay = `${ animationDelayTotal }s`;
 			animationDelayTotal -= animationDelayDeduct;
 		})
 
-		this.addListenerMulti(this.popupLove, 'webkitAnimationEnd animationend', that.resetValueOne.bind(that));
+		this.addListenerMulti(this.popupLove, 'webkitAnimationEnd animationend', this.resetValueOne.bind(this));
 	}
 
 	resetValueOne() {
@@ -183,33 +174,31 @@ class Animations {
 	}
 
 	valueTwo() {
-		const that = this;  
-
 		this.addOrRemoveClass(this.valuePlayButton[1], 'animation--hide', 'add');
 		
 		this.addOrRemoveClass(this.valueTwoCmsButton, 'animation__value-02-cms-btn--play-animation', 'add');
 
-		this.addListenerMulti(this.valueTwoCmsButton, 'webkitAnimationEnd animationend', function(){
+		this.addListenerMulti(this.valueTwoCmsButton, 'webkitAnimationEnd animationend', () => {
 			let animationDelayTotal = 2.3;
 			const animationDelayDeduct = 0.1;
 
-			that.addOrRemoveClass(that.valueTwoCmsText, 'animation--hide', 'add');
+			this.addOrRemoveClass(this.valueTwoCmsText, 'animation--hide', 'add');
 
-			that.addOrRemoveClass(that.valueTwoCmsImage, 'animation--hide', 'add');
-			that.addOrRemoveClass(that.valueTwoCmsButton, 'animation__value-02-cms-btn--play-animation', 'remove');
-			that.addOrRemoveClass(that.valueTwoCmsButton, 'animation--hide', 'add');
+			this.addOrRemoveClass(this.valueTwoCmsImage, 'animation--hide', 'add');
+			this.addOrRemoveClass(this.valueTwoCmsButton, 'animation__value-02-cms-btn--play-animation', 'remove');
+			this.addOrRemoveClass(this.valueTwoCmsButton, 'animation--hide', 'add');
 			
-			that.addOrRemoveClass(that.valueTwoWebsite, 'animation__value-02-website--play-animation', 'add');
+			this.addOrRemoveClass(this.valueTwoWebsite, 'animation__value-02-website--play-animation', 'add');
 			
-			that.addOrRemoveClass(that.valueTwoWindow, 'animation__value-02-window--play-animation', 'add');
+			this.addOrRemoveClass(this.valueTwoWindow, 'animation__value-02-window--play-animation', 'add');
 
-			that.addOrRemoveClass(that.valueTwoHearts, 'animation__value-02-hearts--play-animation', 'add');
-			that.valueTwoHearts.forEach(function(item, index, arr) {
+			this.addOrRemoveClass(this.valueTwoHearts, 'animation__value-02-hearts--play-animation', 'add');
+			this.valueTwoHearts.forEach((item, index, arr) => {
 				item.style.animationDelay = `${ animationDelayTotal }s`;
 				animationDelayTotal += animationDelayDeduct;
 				
 				if (Object.is(arr.length - 1, index)) {
-					that.addListenerMulti(item, 'webkitAnimationEnd animationend', that.resetValueTwo.bind(that));
+					this.addListenerMulti(item, 'webkitAnimationEnd animationend', this.resetValueTwo.bind(this));
 				}
 			})	
 		})
@@ -228,7 +217,6 @@ class Animations {
 	}
 
 	valueThree() {
-		const that = this;
 		let animationDelayTotal = 0;
 		const animationDelay = 0.2;
 
@@ -236,19 +224,19 @@ class Animations {
 
 		this.addOrRemoveClass(this.valueThreeBrowsers, 'animation__value-03-browsers--play-animation', 'add');
 		this.addOrRemoveClass(this.valueThreeBolts, 'animation__value-03-bolts--play-animation', 'add');
-		this.valueThreeBrowsers.forEach(function(item, index, arr) {
+		this.valueThreeBrowsers.forEach(item => {
 			item.style.animationDelay = `${ animationDelayTotal }s`;
 			animationDelayTotal += animationDelay;
 		})
 
-		that.addOrRemoveClass(that.valueThreeMainBrowser, 'animation__value-03-main-browser--play-animation', 'add');
+		this.addOrRemoveClass(this.valueThreeMainBrowser, 'animation__value-03-main-browser--play-animation', 'add');
 		
-		this.addListenerMulti(this.valueThreeMainBrowser, 'webkitAnimationEnd animationend', function(){
-			that.addOrRemoveClass(that.valueThreeMainBolt, 'animation__value-03-main-bolt--play-animation', 'add');
+		this.addListenerMulti(this.valueThreeMainBrowser, 'webkitAnimationEnd animationend', () => {
+			this.addOrRemoveClass(this.valueThreeMainBolt, 'animation__value-03-main-bolt--play-animation', 'add');
 		})
 
-		this.addListenerMulti(this.valueThreeMainBolt, 'webkitAnimationEnd animationend', function(){
-			that.addOrRemoveClass(that.valueThreeWebsite, 'animation__value-03-website--play-animation', 'add');
+		this.addListenerMulti(this.valueThreeMainBolt, 'webkitAnimationEnd animationend', () => {
+			this.addOrRemoveClass(this.valueThreeWebsite, 'animation__value-03-website--play-animation', 'add');
 		})
 
 		this.addListenerMulti(this.valueThreeBolts[0], 'webkitAnimationEnd animationend', this.resetValueThree.bind(this));
@@ -264,33 +252,37 @@ class Animations {
 	}
 
 	addListenerMulti(selector, events, cb) {
-		events.split(' ').forEach(e => selector.addEventListener(e, cb, false));
+		events.split(' ').forEach(event => selector.addEventListener(event, cb, false));
 	}
 
 	addOrRemoveClass(selector, className, specifier) {
 		// Check if selector is an array
 		if(Array.isArray(selector)) {
 			// Loop through array
-			selector.forEach(function(item) {
+			selector.forEach(item => {
 				// Check if each item in array is a nodelist
 				if(NodeList.prototype.isPrototypeOf(item)) {
 					// Check if specifier is add or remove
 					if(specifier === 'add') {
 						// Loop through nodelist and add or remove classname
-						item.forEach(function(element) {
+						item.forEach(element => {
 							element.classList.add(className);
 						})
-					} else {
-						item.forEach(function(element) {
+					} else if (specifier === 'remove') {
+						item.forEach(element => {
 							element.classList.remove(className);
 						})
+					} else {
+						console.log('Specifier must be add or remove. Current: ' + specifier);
 					}
 				// If selector is not a nodelist, add or remove classname directly to selector	
 				} else {
 					if(specifier === 'add') {
 						item.classList.add(className);
-					} else {
+					} else if (specifier === 'remove') {
 						item.classList.remove(className);
+					} else {
+						console.log('Specifier must be add or remove. Current: ' + specifier);
 					}
 				}
 			})
@@ -299,20 +291,24 @@ class Animations {
 			// Check if specifier is add or remove
 			if(specifier === 'add') {
 				// Loop through nodelist and add or remove classname
-				selector.forEach(function(element) {
+				selector.forEach(element => {
 					element.classList.add(className);
 				})
-			} else {
-				selector.forEach(function(element) {
+			} else if (specifier === 'remove') {
+				selector.forEach(element => {
 					element.classList.remove(className);
 				})
+			} else {
+				console.log('Specifier must be add or remove. Current: ' + specifier);
 			}
 		// If selector is not a nodelist, add or remove classname directly to selector	
 		} else {
 			if(specifier === 'add') {
 				selector.classList.add(className);
-			} else {
+			} else if (specifier === 'remove') {
 				selector.classList.remove(className);
+			} else {
+				console.log('Specifier must be add or remove. Current: ' + specifier);
 			}
 		}
 	}
